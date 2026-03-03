c55eabfda21eec76a0e8551b7ca606b6589f9d3e
Jose Ramon Hurtado Mije <jhurtadomije@gmail.com>
2026-03-03 19:05:47 +0100

    final

--- app/(public-pages)/layout.js
+++ app/(public-pages)/layout.js
@@ -2,10 +2,10 @@ import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 
 export const metadata = {
-    title: 'Genesis - PrebuiltUI',
+    title: 'Think-Fast-Show - Live Improvised Theatre',
     description: 'Design AI assistants that research, plan, and execute tasks — all powered by your prompts.',
     appleWebApp: {
-        title: 'Genesis - PrebuiltUI',
+        title: 'Think-Fast-Show',
     },
 };
 
